2fb55e739d01d8365bea32c5c1673b84d9154eeb
RedF <philllip347@gmail.com>
2019-04-03 09:31:56 +0200

    save

---
+++ TPGarage.Client/Converters/ObjectToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TPGarage.Client.Converters
+{
+    class ObjectToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) 
+            => value != null && value != DependencyProperty.UnsetValue ? Visibility.Visible : Visibility.Collapsed;
+        /*{
+            Visibility result = Visibility.Collapsed;
+            if(value != null && value != DependencyProperty.UnsetValue)
+            {
+                result = Visibility.Visible;
+            }
+            return result;
+        }*/
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
